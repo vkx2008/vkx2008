71eaeddbd05f78fc8c0a82e066f48339b4111fd1
vkx2008 <vikax2008@yahoo.com>
2025-06-28 14:36:16 +0500

    scaffold: Python CLI project

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # vkx2008
+
+Python CLI-проект.
+
+## Запуск
+
+```bash
+python -m vkx2008
---
+++ src/vkx2008/cli.py
@@ -0,0 +1,5 @@
+def main():
+    print("Привет из CLI vkx2008!")
+
+if __name__ == "__main__":
+    main()
